edee0c4b3f922a6a9452313354cc9fa2db65add5
Boki <bojan.sejnjanovic@gmail.com>
2026-05-06 19:17:53 +0200

    add the rest of the methods and tests

--- src/hashMap.js
+++ src/hashMap.js
@@ -1,40 +1,113 @@
 class HashMap {
-  loadFactor = 0.75
-  capacity = 16
-  
+  loadFactor = 0.75;
+  capacity = 16;
+  buckets = 0;
+
   constructor() {
-    this.buckets = Array.from({ length: this.capacity}, () => [])
+    this._initiateBuckets();
+  }
+
+  _initiateBuckets() {
+    this.buckets = Array.from({ length: this.capacity }, () => []);
   }
 
   hash(key) {
     let hashCode = 0;
-  
+
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
       hashCode = hashCode % this.capacity;
     }
-  
+
     return hashCode;
   }
-  
-  set(key, value) {
-    const hashedKey = this.hash(key)
-    const bucket = this.buckets[hashedKey]
-    const item = bucket.find(pair => Object.hasOwn(pair, key))
 
-    if (item) item[key] = value
-    else bucket.push({ [key]: value })
+  _findBucket(key) {
+    const index = this.hash(key);
+
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
+
+    return this.buckets[index];
+  }
+
+  set(key, value) {
+    const bucket = this._findBucket(key);
+    const entry = bucket.find((pair) => Object.hasOwn(pair, key));
+
+    if (entry) entry[key] = value;
+    else bucket.push({ [key]: value });
+
+    const threshhold = this.loadFactor * this.capacity;
+    if (this.length() > threshhold) this._growMap();
   }
 
   get(key) {
-    const hashedKey = this.hash(key)
-    const bucket = this.buckets[hashedKey]
-    const item = bucket.find(pair => Object.hasOwn(pair, key))
+    const bucket = this._findBucket(key);
+    const entry = bucket.find((pair) => Object.hasOwn(pair, key));
+    return entry ? entry[key] : null;
+  }
 
-    const value = (item) ? item[key] : null
-    return value
+  has(key) {
+    const bucket = this._findBucket(key);
+    const entry = bucket.find((pair) => Object.hasOwn(pair, key));
+    return entry ? true : false;
+  }
+
+  remove(key) {
+    const bucket = this._findBucket(key);
+    const index = bucket.findIndex((entry) => Object.hasOwn(entry, key));
+
+    if (index !== -1) {
+      bucket.splice(index, 1);
+      return true;
+    } else return false;
+  }
+
+  length() {
+    return this.buckets.reduce((prev, curr) => {
+      return prev + curr.length;
+    }, 0);
+  }
+
+  clear() {
+    this.buckets.forEach((bucket) => {
+      bucket.length = 0;
+    });
+  }
+
+  _fetch(func) {
+    const array = [];
+    this.buckets.forEach((bucket) => {
+      bucket.forEach((entry) => {
+        const item = func(entry);
+        array.push(...item);
+      });
+    });
+
+    return array;
+  }
+
+  keys() {
+    return this._fetch(Object.keys);
+  }
+
+  values() {
+    return this._fetch(Object.values);
+  }
+
+  entries() {
+    return this._fetch(Object.entries);
+  }
+
+  _growMap() {
+    this.capacity *= 2;
+    const entries = this.entries();
+    this._initiateBuckets();
+    entries.forEach((entry) => this.set(entry[0], entry[1]));
   }
 }
 
-export { HashMap }
+export { HashMap };
--- test/hashMap.test.js
+++ test/hashMap.test.js
@@ -2,21 +2,32 @@ import { HashMap } from "../src/hashMap";
 
 describe("HashMap methods testing", () => {
   let map;
+  let filledMap;
 
   beforeEach(() => {
     map = new HashMap();
+
+    filledMap = new HashMap();
+    filledMap.set("apple", "red");
+    filledMap.set("banana", "yellow");
+    filledMap.set("carrot", "orange");
+    filledMap.set("dog", "brown");
+    filledMap.set("elephant", "gray");
+    filledMap.set("frog", "green");
+    filledMap.set("grape", "purple");
+    filledMap.set("hat", "black");
+    filledMap.set("ice cream", "white");
+    filledMap.set("jacket", "blue");
+    filledMap.set("kite", "pink");
+    filledMap.set("lion", "golden");
   });
 
   test("hash: should always return same values for a key", () => {
-    const a1 = map.hash("apple")
-    const a2 = map.hash("apple")
-    const isEqual = a1 === a2
-
-    let b1 = map.hash("Rama")
-    let b2 = map.hash("Sita")
-    console.log(a1, a2, b1, b2)
-    expect(isEqual).toBe(true)
-  })
+    const a1 = map.hash("apple");
+    const a2 = map.hash("apple");
+    const isEqual = a1 === a2;
+    expect(isEqual).toBe(true);
+  });
 
   test("initiation: should create an array with capacity number of arrays", () => {
     expect(map.buckets).toEqual([
@@ -91,7 +102,7 @@ describe("HashMap methods testing", () => {
     map.set("apple", "red");
     map.set("banana", "yellow");
     map.set("Rama", "green");
-    map.set("Sita", "yellow")
+    map.set("Sita", "yellow");
     expect(map.buckets.sort()).toEqual([
       [],
       [],
@@ -108,17 +119,139 @@ describe("HashMap methods testing", () => {
       [],
       [{ banana: "yellow" }],
       [{ apple: "red" }],
-      [{ Rama: "green"}, { Sita: "yellow"}],
+      [{ Rama: "green" }, { Sita: "yellow" }],
     ]);
   });
 
   test("get: should return value for the provided key", () => {
-    map.set("apple", "red");
-    expect(map.get("apple")).toBe("red")
-  })
+    expect(filledMap.get("apple")).toBe("red");
+  });
 
   test("get: should return null for the provided key if it is missing", () => {
     map.set("apple", "red");
-    expect(map.get("banana")).toBe(null)
-  })
+    expect(filledMap.get("pineapple")).toBe(null);
+  });
+
+  test("has: should return false if key is not in the hash map", () => {
+    expect(filledMap.has("strawberry")).toBe(false);
+  });
+
+  test("has: should return true if key is in the hash map", () => {
+    expect(filledMap.has("carrot")).toBe(true);
+  });
+
+  test("remove: should return false if key is not in the hash map", () => {
+    expect(filledMap.remove("aPPLe")).toBe(false);
+  });
+
+  test("remove: should return true and remove entry from hash map if key is in the hash map", () => {
+    expect(filledMap.remove("banana")).toBe(true);
+    expect(filledMap.has("banana")).toBe(false);
+  });
+
+  test("length: should return correct number of keys in the buckets", () => {
+    expect(map.length()).toBe(0);
+    expect(filledMap.length()).toBe(12);
+  });
+
+  test("clear: should remove all entries from the map", () => {
+    filledMap.clear();
+    expect(filledMap.length()).toBe(0);
+  });
+
+  test("keys: should return an array containing all the keys from hash map", () => {
+    expect(map.keys()).toEqual([]);
+    expect(filledMap.keys().sort()).toEqual(
+      [
+        "apple",
+        "banana",
+        "carrot",
+        "dog",
+        "elephant",
+        "frog",
+        "grape",
+        "hat",
+        "ice cream",
+        "jacket",
+        "kite",
+        "lion",
+      ].sort()
+    );
+  });
+
+  test("values: should return an array containing all the values from hash map", () => {
+    expect(map.values()).toEqual([]);
+    expect(filledMap.values().sort()).toEqual(
+      [
+        "red",
+        "yellow",
+        "orange",
+        "brown",
+        "gray",
+        "green",
+        "purple",
+        "black",
+        "white",
+        "blue",
+        "pink",
+        "golden",
+      ].sort()
+    );
+  });
+
+  test("entries: should return an array containing arrays of key value pairs from hash map", () => {
+    expect(map.entries()).toEqual([]);
+    expect(filledMap.entries().sort()).toEqual(
+      [
+        ["apple", "red"],
+        ["banana", "yellow"],
+        ["carrot", "orange"],
+        ["dog", "brown"],
+        ["elephant", "gray"],
+        ["frog", "green"],
+        ["grape", "purple"],
+        ["hat", "black"],
+        ["ice cream", "white"],
+        ["jacket", "blue"],
+        ["kite", "pink"],
+        ["lion", "golden"],
+      ].sort()
+    );
+  });
+
+  test("growth: setting new values, should not trigger growth before threshold", () => {
+    filledMap.set("apple", "green");
+    filledMap.set("elephant", "pink");
+    filledMap.set("lion", "hungry");
+    expect(filledMap.capacity).toBe(16);
+  });
+
+  test("growth: should double capacity after threshhold has been passed and keep all entries", () => {
+    filledMap.set("fox", "sneaky");
+    expect(filledMap.length()).toBe(13);
+    expect(filledMap.capacity).toBe(32);
+  });
+
+  test("growth: rest of the methods should be working after growth", () => {
+    filledMap.remove("lion");
+    filledMap.set("fox", "sneaky");
+    filledMap.set("snake", "sssss");
+    filledMap.set("tiger", "meooow");
+    expect(filledMap.capacity).toBe(32);
+    expect(filledMap.get("banana")).toBe("yellow");
+    expect(filledMap.has("lion")).toBe(false);
+    expect(filledMap.get("snake")).toBe("sssss");
+
+    filledMap.clear();
+    expect(filledMap.length()).toBe(0);
+  });
+
+  test("growth: triggers after second threshold has been passed", () => {
+    for (let i = 0; i < 14; i++) {
+      filledMap.set(`key ${i}`, `value ${i}`);
+    }
+
+    expect(filledMap.capacity).toBe(64);
+    expect(filledMap.length()).toBe(26);
+  });
 });
